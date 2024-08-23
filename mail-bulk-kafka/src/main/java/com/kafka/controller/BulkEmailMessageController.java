b50fb222d5efb7e5430c69076e3fe16dea937102
duyphuong205 <phuongvdd@seatechit.com.vn>
2024-08-23 15:57:25 +0700

    config log server

--- mail-bulk-kafka/src/main/java/com/kafka/controller/BulkEmailMessageController.java
+++ mail-bulk-kafka/src/main/java/com/kafka/controller/BulkEmailMessageController.java
@@ -5,6 +5,8 @@ import com.kafka.pojo.DTO.BulkEmailMessageDTO;
 import com.kafka.pojo.response.DataResponse;
 import com.kafka.service.BulkEmailMessageService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1-bulk-email-messages")
 public class BulkEmailMessageController {
+    private static final Logger logger = LogManager.getLogger(BulkEmailMessageController.class);
 
     private final BulkEmailMessageService bulkEmailMessageService;
 
     @PostMapping
     public ResponseEntity<?> doCreateEmailMessage(@RequestBody BulkEmailMessageDTO emailMessageDTO) {
+        logger.info("**********BulkEmailMessageController Begin call API**********");
         bulkEmailMessageService.create(emailMessageDTO);
         DataResponse dataResponse = DataResponse.builder()
                 .code(AppConstants.CODE_SUCCESS)
                 .message(AppConstants.MESSAGE_SUCCESS)
                 .build();
+        logger.info("**********BulkEmailMessageController End call API**********");
         return new ResponseEntity<>(dataResponse, HttpStatus.CREATED);
     }
 }
--- mail-bulk-kafka/src/main/java/com/kafka/controller/EmailMessageController.java
+++ mail-bulk-kafka/src/main/java/com/kafka/controller/EmailMessageController.java
@@ -5,6 +5,8 @@ import com.kafka.pojo.DTO.EmailMessageDTO;
 import com.kafka.pojo.response.DataResponse;
 import com.kafka.service.EmailMessageService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1-email-messages")
 public class EmailMessageController {
+    private static final Logger logger = LogManager.getLogger(EmailMessageController.class);
 
     private final EmailMessageService emailMessageService;
 
     @PostMapping
     public ResponseEntity<?> doCreateEmailMessage(@RequestBody EmailMessageDTO emailMessageDTO) {
+        logger.info("**********EmailMessageController Begin call API**********");
         emailMessageService.create(emailMessageDTO);
         DataResponse dataResponse = DataResponse.builder()
                 .code(AppConstants.CODE_SUCCESS)
                 .message(AppConstants.MESSAGE_SUCCESS)
                 .build();
+        logger.info("**********EmailMessageController End call API**********");
         return new ResponseEntity<>(dataResponse, HttpStatus.CREATED);
     }
 }
